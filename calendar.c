538648846ef383182b7dea68ea03547947ec4f0e
jungsoojun <jdyun120032naver.com>
2017-05-01 16:38:42 +0900

    add test.txt

--- calendar.c
+++
@@ -1,7 +0,0 @@
-#include "diary.h"
-//tet
-int calendar()
-{
-	printf("function calendar.\n");
-	return 0;
-}
--- diary.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-//this is Calender()
-
-int calendar();
-{
-	printf("function calendar.\n");
-	return 0;
-}
